69776efd6c16141e93133515258108ee1d80c8f9
unknown <lanmi992@live.com>
2021-07-01 15:56:43 +0200

    Updates

---
+++ static/js/15.e65bc371.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptour-agency-react"]=this["webpackJsonptour-agency-react"]||[]).push([[15],{151:function(t,c,a){"use strict";a.d(c,"a",(function(){return r}));var n=a(0);function r(t){return Object(n.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z"}}]})(t)}}}]);
+//# sourceMappingURL=15.e65bc371.chunk.js.map
